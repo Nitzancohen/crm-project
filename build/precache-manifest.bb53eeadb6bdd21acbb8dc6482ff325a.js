014669ddd00dfc3ffd705ee07c600aa45f4bbacd
Nitzancohen <nitzancohen105@gmail.com>
2018-11-07 18:21:05 +0200

    heroku setups

---
+++ build/precache-manifest.bb53eeadb6bdd21acbb8dc6482ff325a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c6b37bd7acf7be36b67c",
+    "url": "/static/js/main.c6b37bd7.chunk.js"
+  },
+  {
+    "revision": "0610dbd231413513b7f7",
+    "url": "/static/js/1.0610dbd2.chunk.js"
+  },
+  {
+    "revision": "c6b37bd7acf7be36b67c",
+    "url": "/static/css/main.d08f6b0f.chunk.css"
+  },
+  {
+    "revision": "fa0dc80cd3160c543d314c527728f6a2",
+    "url": "/index.html"
+  }
+];
