cebf56b68220285f20c3854727e010020f5588ad
Nitzancohen <nitzancohen105@gmail.com>
2018-11-08 09:19:32 +0200

    heroku

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const clientApi = require('./clientsApi.js')
 
-app.use(express.static('build'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('build'));
+app.use(express.static('public'));
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
